e6d890b609b99ffa56633d9411c0a7e8103d3618
Stabbyiscool <elocreatorelo@gmail.com>
2024-07-19 18:54:34 -0500

    sss

--- cogs/figlet.py
+++ cogs/figlet.py
@@ -6,15 +6,15 @@ import json
 with open('configs.json', 'r') as config_file:
     config = json.load(config_file)
 
-class ascii(commands.Cog):
+class figlet(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='ascii')
+    @commands.command(name='figlet')
     @commands.cooldown(rate=1, per=config['COOLDOWN_TIME'], type=commands.BucketType.user)
     async def textart(self, ctx, *, request: str):
         result = pyfiglet.figlet_format(request)
         await ctx.reply(f"```\n{result}\n```")
 
 async def setup(bot):
-    await bot.add_cog(ascii(bot))
+    await bot.add_cog(figlet(bot))

f9b2fae9c345e0ee3dc6f9fa974d095cce775950
Stabbyiscool <elocreatorelo@gmail.com>
2024-07-19 18:00:05 -0500

    Create buildaboogian.py

---
+++ cogs/buildaboogian.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import json
+import tempfile
+import os
+with open('configs.json', 'r') as config_file:
+    config = json.load(config_file)
+
+class buildaboogian(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='buildaboogian')
+    @commands.cooldown(rate=1, per=config['COOLDOWN_TIME'], type=commands.BucketType.user)
+    async def eight_ball(self, ctx, *, question: str):
+        await ctx.send('[Boogian offical repo](https://github.com/Stabbyiscool/Boogian)')
+
+    @eight_ball.error
+    async def handle_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            return
+        else:
+            await ctx.send("Command no workey ping stabby")
+
+async def setup(bot):
+    await bot.add_cog(buildaboogian(bot))
